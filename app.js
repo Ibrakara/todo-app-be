a26f3b53d50d19d7070c3db81ab967e2586db04c
ibrahim <ibr.karaduman@gmail.com>
2025-05-06 12:19:58 +0300

    initialize Express application with MongoDB connection and Todo routes

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { router as todoRoutes } from "./routes/todos.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use("/api/todos", todoRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.error(err));
